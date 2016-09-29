4c19f78a83663f7c7b002c28fdb0fdc7e5373146
agdavid <antonio.g.david@gmail.com>
2016-09-29 15:54:48 -0400

    add standard index/show routes to StatesController

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -2,10 +2,14 @@ class StatesController < ApplicationController
 
   def index
     # get /states will go to states#index and return all states
+    states = State.all
+    render json: states 
   end
 
   def show
     # get /states/:slug will go to states#show and return a state
+    state = State.find(params[:id])
+    render json: state
   end
 
 end
