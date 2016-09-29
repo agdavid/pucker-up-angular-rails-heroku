6410ba04d729b955f6804e11d87b91b69b3e7ec3
agdavid <antonio.g.david@gmail.com>
2016-09-29 15:29:22 -0400

    States controller and routes

---
+++ app/controllers/states_controller.rb
@@ -0,0 +1,11 @@
+class StatesController < ApplicationController
+
+  def index
+    # get /states will go to states#index and return all states
+  end
+
+  def show
+    # get /states/:slug will go to states#show and return a state
+  end
+
+end
