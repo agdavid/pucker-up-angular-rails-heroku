d7a3e37d436f40189c295f93b405fb3bc5dd1151
agdavid <antonio.g.david@gmail.com>
2016-09-29 16:50:22 -0400

    update ApplicationController to respond_to JSON

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,24 @@ require "application_responder"
 
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
-  respond_to :html
 
   protect_from_forgery with: :exception
 
   after_filter :set_csrf_cookie
 
+  respond_to :json
+
   def set_csrf_cookie
-    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+      cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
   end
 
   def index
-    # root application#index will be the home to ui-view
+      # root application#index will be the home to ui-view
   end
 
   protected
-    def verified_request?
-      super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
-    end
+      def verified_request?
+          super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+      end
 
 end
