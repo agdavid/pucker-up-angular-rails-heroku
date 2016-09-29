f43ff2d999ed9cc2b8127957f68f82a54b3edc62
agdavid <antonio.g.david@gmail.com>
2016-09-29 15:51:38 -0400

    add standard CRUD routes to BreweriesController

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -2,22 +2,45 @@ class BreweriesController < ApplicationController
 
   def index
     # get /breweries will go to breweries#index and return all breweries
+    breweries = Brewery.all
+    render json: breweries 
   end
 
   def create
     # post /breweries will go to breweries#create and instantiate a brewery
+    brewery = Brewery.new(brewery_params)
+    if brewery.save
+      render json: brewery
+    else
+      render json: brewery 
+    end
   end
 
   def show
     # get /breweries/:id will go to breweries#show and return a brewery
+    brewery = Brewery.find(params[:id])
+    render json: brewery
   end
 
   def update
     # patch /breweries/:id will go to breweries#update and edit a brewery
+    brewery = Brewery.find(params[:id])
+    if brewery.update(brewery_params)
+      render json: brewery 
+    else
+      render json: brewery 
+    end
   end
 
   def delete
     # post with method of delete /breweries/:id will go to breweries#destroy and delete a brewery
+    brewery = Brewery.find(params[:id])
+    brewery.destroy
   end
+
+  private
+    def brewery_params
+      params.require(:brewery).permit(:name, :state_id)
+    end
 
 end
