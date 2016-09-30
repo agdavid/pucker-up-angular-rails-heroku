bc8afee535223c9c54d77ebf4af6038bedf6f97e
agdavid <antonio.g.david@gmail.com>
2016-09-30 12:03:38 -0400

    add StateFactory and getStates() AJAX

---
+++ app/assets/javascripts/states/states.factory.js
@@ -0,0 +1,32 @@
+(function(){
+
+    'use strict';
+
+    function StateFactory($http) {
+
+        return {
+            getStates: getStates;
+        }
+
+        function getStates() {
+            return $http.get('/states')
+                .then(handleSuccess)
+                .catch(handleError)
+        };
+
+        function handleSuccess(response) {
+            console.log(response);
+            return response.data;
+        };
+
+        function handleError(error) {
+            console.log(error);
+        };
+
+    };
+
+  angular
+      .module('app')
+      .factory('StateFactory', StateFactory);
+
+}());
