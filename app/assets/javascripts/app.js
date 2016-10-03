3be33730a547b18ce6d2b687eb2474c94a7b957f
agdavid <antonio.g.david@gmail.com>
2016-10-03 15:34:47 -0400

    add ngMessages module to pipeline, JS manifest, and root app

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,7 +3,7 @@
   'use strict';
 
   angular
-      .module('app', ['ui.router', 'templates'])
+      .module('app', ['ui.router', 'templates', 'ngMessages'])
       .config(function($httpProvider) {
           // for CSRF errors
           $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,5 @@
 //= require angular
 //= require angular-ui-router
 //= require angular-rails-templates
+//= require angular-messages
 //= require_tree .
