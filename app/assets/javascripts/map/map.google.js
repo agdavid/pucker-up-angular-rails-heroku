6a240d3493e19db7ab17da1001632c7b3fb12ca9
agdavid <antonio.g.david@gmail.com>
2016-10-04 15:27:25 -0400

    begin adapting map for Breweries - center on USA

--- app/assets/javascripts/map/map.google.js
+++ app/assets/javascripts/map/map.google.js
@@ -1,12 +1,13 @@
 // Sample GoogleMap to confirm working properly 
 function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
+        // Center map on USA
+        var usa = {lat: 41.560654, lng: -98.491622};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
-          center: uluru
+          center: usa
         });
         var marker = new google.maps.Marker({
-          position: uluru,
+          position: usa,
           map: map
         });
       }
