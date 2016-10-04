f65ec4c1aeedcfd77983d7ec958ad4bf2f901e02
agdavid <antonio.g.david@gmail.com>
2016-10-03 20:23:41 -0400

    add 'angular-devise' via Bower for Angular frontend authentication

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require angular
+//= require angular-devise
 //= require angular-ui-router
 //= require angular-rails-templates
 //= require angular-messages
