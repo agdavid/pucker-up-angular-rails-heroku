ee5b2e325e9f1d221490474557e3615ae21430ac
agdavid <antonio.g.david@gmail.com>
2016-10-04 11:37:58 -0400

    cleanup routes

--- app/assets/javascripts/app.routes.js
+++ app/assets/javascripts/app.routes.js
@@ -14,39 +14,39 @@
               })
           //static pages
               .state('home.faq', {
-                  url: '/faq',
+                  url: 'faq',
                   templateUrl: 'faq/home.html'
               })
               .state('home.faq.history', {
-                  url:'/history',
+                  url:'history',
                   templateUrl: 'faq/history.html'
               })
               .state('home.faq.science', {
-                  url: '/science',
+                  url: 'science',
                   templateUrl: 'faq/science.html'
               })
           //breweries pages
               .state('home.breweries', {
-                url: '/breweries',
+                url: 'breweries',
                 templateUrl: 'breweries/home.html'
               })
               .state('home.breweries.index', {
-                url: '/index',
+                url: 'index',
                 templateUrl: 'breweries/index.html',
                 controller: 'BreweriesController as breweriesCtrl'
               })
               .state('home.breweries.create', {
-                url: '/create',
+                url: 'create',
                 templateUrl: 'breweries/create.html',
                 controller: 'BreweriesController as breweriesCtrl'
               })
               .state('home.breweries.edit', {
-                url: '/edit/:breweryId',
+                url: 'edit/:breweryId',
                 templateUrl: 'breweries/edit.html',
                 controller: 'BreweriesShowController as breweriesShowCtrl'
               })
               .state('home.breweries.show', {
-                url: '/show/:breweryId',
+                url: 'show/:breweryId',
                 templateUrl: 'breweries/show.html',
                 controller: 'BreweriesShowController as breweriesShowCtrl'
               });
