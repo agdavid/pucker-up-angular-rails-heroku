8fe3661850c66a288835c3ab3398ad7926e4b7e7
agdavid <antonio.g.david@gmail.com>
2016-10-04 15:30:52 -0400

    add JS construction function for Coordinate of each Brewery

--- app/assets/javascripts/map/map.google.js
+++ app/assets/javascripts/map/map.google.js
@@ -1,13 +1,22 @@
 // Sample GoogleMap to confirm working properly 
 function initMap() {
-        // Center map on USA
-        var usa = {lat: 41.560654, lng: -98.491622};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: usa
-        });
-        var marker = new google.maps.Marker({
-          position: usa,
-          map: map
-        });
-      }
+    // Center map on USA
+    var usa = {lat: 41.560654, lng: -98.491622};
+        
+    // Instantiate new Map
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: usa
+    });
+
+    // JS model object
+    function Coordinate(id, name, url, lat, lng) {
+        this.id = id
+        this.name = name
+        this.url = url
+        this.lat = lat
+        this.lng = lng
+    };
+
+
+};
