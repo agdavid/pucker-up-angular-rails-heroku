8344b3cabb2a23e25bde325d4f4bff80704f4a82
agdavid <antonio.g.david@gmail.com>
2016-10-14 15:35:57 -0400

    add function to set stateName and getBreweries

--- app/assets/javascripts/states/states.show.controller.js
+++ app/assets/javascripts/states/states.show.controller.js
@@ -2,9 +2,35 @@
 
     'use strict';
 
-    function StatesShowController() {
+    function StatesShowController($stateParams, BreweryFactory, $filter) {
 
-    }
+        var vm = this;
+
+        activate();
+
+        function activate() {
+            setStateName($stateParams);
+            getBreweries();
+            
+        };
+
+        function setStateName($stateParams) {
+            var stateLettersArray = $stateParams.stateName.split('');
+            var upperCaseLetter = stateLettersArray[0].toUpperCase();
+            stateLettersArray[0] = upperCaseLetter;
+            return vm.stateName = stateLettersArray.join('');
+        };
+
+        function getBreweries() {
+            BreweryFactory.getBreweries()
+                          .then(setBreweries);
+        };
+
+        function setBreweries(data) {
+            return vm.breweries = data;
+        };
+
+    };
 
     angular
         .module('app')
