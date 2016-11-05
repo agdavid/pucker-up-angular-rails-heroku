42e1933c4be9fddc0399f140ce7a7a80cbed9ebc
agdavid <antonio.g.david@gmail.com>
2016-11-05 07:15:07 -0400

    add StatesController skeleton specs

---
+++ spec/controllers/states_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe StatesController do 
+
+  describe 'GET #index' do 
+      it "populates an array of all states"
+      it "returns JSON"
+  end
+  describe 'GET #show' do 
+      it "assigns the requested state to @state"
+      it "returns JSON"
+  end
+end
