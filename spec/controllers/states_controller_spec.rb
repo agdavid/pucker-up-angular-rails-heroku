7b149623c0a5e7027bf4774f7d19c9dd503a4a18
agdavid <antonio.g.david@gmail.com>
2016-11-05 08:11:32 -0400

    complete states#show controller and api request test

--- spec/controllers/states_controller_spec.rb
+++ spec/controllers/states_controller_spec.rb
@@ -14,8 +14,12 @@ describe StatesController do
   end
   
   describe 'GET #index' do 
-      it "populates an array of all states"
-      it "returns JSON"
+      it "assigns an array of all states to @states" do 
+          get :index
+          expect(assigns(:states).count).to eq(2)
+          expect(assigns(:states)).to match_array([@georgia, @oregon])
+      end
+      it "returns all states as JSON" #TO DO
   end
   
   describe 'GET #show' do 
@@ -23,7 +27,13 @@ describe StatesController do
           get :show, id: @georgia
           expect(assigns(:state)).to eq(@georgia)
       end
-      it "returns JSON"
+      it "returns a state as JSON" do 
+          get :show, id: @georgia
+          expect(response).to have_http_status(:success)
+          json = JSON.parse(response.body)
+          expect(json['name']).to eq("Georgia")
+          expect(json['abbreviation']).to eq("GA")
+      end
   end
 
 end
